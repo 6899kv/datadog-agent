6b071e9043c5b87a928628581578a6e6aa542ea5
Minyi Zhu <minyi.zhu@datadoghq.com>
2024-01-11 16:09:28 -0500

    Add tagger module to agent flare and diagnose

--- cmd/agent/subcommands/diagnose/command.go
+++ cmd/agent/subcommands/diagnose/command.go
@@ -20,6 +20,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/log"
 	"github.com/DataDog/datadog-agent/comp/core/log/logimpl"
+	"github.com/DataDog/datadog-agent/comp/core/tagger"
 	"github.com/DataDog/datadog-agent/comp/core/workloadmeta"
 	"github.com/DataDog/datadog-agent/comp/core/workloadmeta/collectors"
 	"github.com/DataDog/datadog-agent/pkg/api/util"
@@ -95,6 +96,7 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 					}
 				}),
 				workloadmeta.OptionalModule(),
+				tagger.OptionalModule(),
 				diagnosesendermanagerimpl.Module(),
 			)
 		},
@@ -208,7 +210,10 @@ This command print the inventory-checks metadata payload. This payload is used b
 	return []*cobra.Command{diagnoseCommand}
 }
 
-func cmdDiagnose(cliParams *cliParams, senderManager diagnosesendermanager.Component, _ optional.Option[workloadmeta.Component]) error {
+func cmdDiagnose(cliParams *cliParams,
+	senderManager diagnosesendermanager.Component,
+	_ optional.Option[workloadmeta.Component],
+	_ optional.Option[tagger.Component]) error {
 	diagCfg := diagnosis.Config{
 		Verbose:  cliParams.verbose,
 		RunLocal: cliParams.runLocal,
